b5191555d9d522a9da2190430104164db8ef828e
Nghi Dang <dangquocnghi@gmail.com>
2022-06-11 16:14:00 +0700

    Add icon components

---
+++ src/icons/DrawerMenuIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Svg, {Path} from 'react-native-svg';
+
+type Props = {
+  color: string;
+  size: number;
+};
+
+const DrawerMenuIcon = (props: Props) => {
+  return (
+    <Svg viewBox="0 0 512 512" width={props.size} height={props.size}>
+      <Path
+        fill="none"
+        stroke={props.color}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth={32}
+        d="M80 160h352M80 256h352M80 352h352"
+      />
+    </Svg>
+  );
+};
+
+export default DrawerMenuIcon;
